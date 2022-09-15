8ac12dc87dafdcb1fb9e24c8f716008108426392
Daniel Santaguida <daniel.santaguida@unity3d.com>
2022-09-15 13:45:11 -0400

    added cutscene skip toggle

--- Packages/com.unity.skillscompetition.objects/Runtime/Scripts/States/CutsceneState.cs
+++ Packages/com.unity.skillscompetition.objects/Runtime/Scripts/States/CutsceneState.cs
@@ -6,19 +6,25 @@ namespace Scripts.States
 {
     public class CutsceneState : GameState
     {
+        
+        private const string PlayerPrefsCutsceneKey = "disableCutscene";
+
         public CutsceneState(GameStateManager context) : base(context)
         {
         }
 
         public override void Enter()
         {
+            if (PlayerPrefs.GetInt(PlayerPrefsCutsceneKey) == 1)
+            {
+                Context.SwitchState(new PlayState(Context));
+            }
         }
 
         public override void Update()
         {
             if (Math.Abs(Context.dollyCart.m_Position - Context.dollyCart.m_Path.PathLength) <= 0.2f) 
                 Context.SwitchState(new PlayState(Context));
-            
         }
 
         public override void Exit()
